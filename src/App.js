9d887c9e4d21d1a4934cb1e858a590ec358cded1
Bethany Griffin <bethanyjeanbg@gmail.com>
2022-07-23 18:30:31 -0400

    ready to test

--- src/App.js
+++ src/App.js
@@ -1,23 +1,29 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
+import Nav from './components/Nav';
+import About from './components/About';
+import ProjectList from './components/ProjectList';
+import ContactForm from './components/Contact';
 
 function App() {
+  const [contactSelected, setContactSelected] = useState(false);
+  const [projectSelected, setProjectSelected] = useState(false);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Nav
+        contactSelected={contactSelected}
+        projectSelected={projectSelected}
+      ></Nav>
+    <main>
+    {!contactSelected ? (
+          <>
+            <ProjectList></ProjectList>
+            <About></About>
+          </>
+        ) : (
+          <ContactForm></ContactForm>
+        )}
+    </main>
     </div>
   );
 }
